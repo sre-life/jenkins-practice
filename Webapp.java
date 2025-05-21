2f22a25853ff1a26a831a2cda61ebbb483fd9164
Francisco Tavarez <francisco.tavarezveras@example.com>
2025-05-21 10:34:40 -0400

    upload another java web app

--- Webapp.java
+++
@@ -1,8 +0,0 @@
-public class Webapp
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
-    }
-}
-
---
+++ webapp.java
@@ -0,0 +1,8 @@
+public class webapp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Devops People..");
+    }
+}
+
